25799c97763d8fdd00408793d0fb8102cf53aa21
Heiler Nova <heilernova@outlook.com>
2025-09-05 15:44:37 -0500

    feat(api): agregar funciones para obtener la información de una cuenta contable

--- apps/api/src/app/models/puc/puc.service.ts
+++ apps/api/src/app/models/puc/puc.service.ts
@@ -40,6 +40,29 @@ export class PucService {
     return this.pucData;
   }
 
+  public async getById(code: string): Promise<PUCEntry | null> {
+    return (await this._dbConnectionService.query('SELECT code, name FROM data_puc WHERE code = $1', [code])).rows[0] ?? null;
+  }
+
+  public async getParent(code: string): Promise<PUCEntry[]> {
+    const pairs = this.splitInPairs(code);
+    const list: string[] = [];
+    pairs.forEach(part => {
+      const last = list[list.length - 1] || '';
+      list.push(last + part);
+    });
+    if (code.length > 1) {
+      list.unshift(code.substring(0, 1));
+    }
+
+    const parent = await this._dbConnectionService.query('SELECT code, name FROM data_puc WHERE code = ANY($1) ORDER BY code ASC', [list]);
+    return parent.rows ?? [];
+  }
+
+  private splitInPairs(str: string): string[] {
+    return str.match(/.{1,2}/g) || [];
+  }
+
   public async update(code: string, name: string): Promise<boolean> {
     const result = await this._dbConnectionService.query('UPDATE data_puc SET name = $1 WHERE code = $2 RETURNING *', [name, code]);
     const updatedRow: { name: string, code: string } | null = result.rows[0] ?? null;
