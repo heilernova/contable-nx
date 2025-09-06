4ce113389bcb335e8842d594367fa43331c53453
Heiler Nova <heilernova@outlook.com>
2025-09-06 08:26:20 -0500

    feat(api): cargar las variables de entorno desde `.env`
    Se implementa la carga de variables de entorno desde el archivo `.env`, incluyendo la verificación de su existencia antes de inicializar la aplicación.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,11 +3,15 @@
  * This is only a minimal backend to get started.
  */
 
+import { existsSync } from 'node:fs';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
+  if (existsSync('.env')) {
+   process.loadEnvFile('.env');
+  }
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
